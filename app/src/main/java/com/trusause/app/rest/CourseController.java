d598ad65c4fe65955eb67d22f0702a7486d3dc91
韦昌龙 <changlong.wei@five-star.cn>
2019-08-29 11:07:51 +0800

    no message

--- app/src/main/java/com/trusause/app/rest/CourseController.java
+++ app/src/main/java/com/trusause/app/rest/CourseController.java
@@ -1,15 +1,14 @@
 package com.trusause.app.rest;
 
 import com.github.pagehelper.PageInfo;
+import com.trusause.app.domain.Course;
 import com.trusause.app.domain.Teacher;
 import com.trusause.app.dto.ResultDTO;
 import com.trusause.app.service.CourseService;
 import com.trusause.app.service.TeacherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: weicl
@@ -28,6 +27,31 @@ public class CourseController {
     @GetMapping("/selectByPrimaryKey")
     public ResultDTO selectByPrimaryKey(Integer id) {
         return ResultDTO.builder().data(courseService.selectByPrimaryKey(id)).build();
+    }
+
+    @PostMapping("/insert")
+    public ResultDTO insert(@RequestBody Course record) {
+        return ResultDTO.builder().data(courseService.insert(record)).build();
+    }
+
+    @PostMapping("/insertSelective")
+    public ResultDTO insertSelective(@RequestBody Course record) {
+        return ResultDTO.builder().data(courseService.insertSelective(record)).build();
+    }
+
+    @PutMapping("/updateByPrimaryKey")
+    public ResultDTO updateByPrimaryKey(@RequestBody Course record) {
+        return ResultDTO.builder().data(courseService.updateByPrimaryKey(record)).build();
+    }
+
+    @PutMapping("/updateByPrimaryKeySelective")
+    public ResultDTO updateByPrimaryKeySelective(@RequestBody Course record) {
+        return ResultDTO.builder().data(courseService.updateByPrimaryKeySelective(record)).build();
+    }
+
+    @DeleteMapping("/deleteByPrimaryKey")
+    public ResultDTO deleteByPrimaryKey(Integer id) {
+        return ResultDTO.builder().data(courseService.deleteByPrimaryKey(id)).build();
     }
 
 }
--- app/src/main/java/com/trusause/app/service/CourseService.java
+++ app/src/main/java/com/trusause/app/service/CourseService.java
@@ -9,6 +9,7 @@ import com.trusause.app.mapper.TeacherMapper;
 import com.trusause.app.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author: weicl
@@ -24,5 +25,30 @@ public class CourseService {
 
     public Course selectByPrimaryKey(Integer id) {
         return courseMapper.selectByPrimaryKey(id);
+    }
+
+    @Transactional
+    public int insert(Course record) {
+        return courseMapper.insert(record);
+    }
+
+    @Transactional
+    public int insertSelective(Course record) {
+        return courseMapper.insertSelective(record);
+    }
+
+    @Transactional
+    public int updateByPrimaryKeySelective(Course record) {
+        return courseMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Transactional
+    public int updateByPrimaryKey(Course record) {
+        return courseMapper.updateByPrimaryKey(record);
+    }
+
+    @Transactional
+    public int deleteByPrimaryKey(Integer id) {
+        return courseMapper.deleteByPrimaryKey(id);
     }
 }
